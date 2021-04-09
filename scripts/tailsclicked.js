bae3dceb320b7465e6ab556806c7f3b25caeb3a8
sdoma <82196574+sdomaGitHub@users.noreply.github.com>
2021-04-09 10:00:04 -0400

    Update tailsclicked.js

--- scripts/tailsclicked.js
+++ scripts/tailsclicked.js
@@ -1,8 +1,8 @@
 (function headsclicked() {
-  document.getElementById("headsclicked").style(".background-color") = green;
-  if (document.getElementById("tailsclicked").style(".background-color") = green) {
-    document.getElementById("headsclicked").style(".background-color") = green;
-    document.getElementById("tailsclicked").style(".background-color") = white;
+  document.getElementById("tailsclicked").style(".background-color") = green;
+  if (document.getElementById("headsclicked").style(".background-color") = green) {
+    document.getElementById("tailsclicked").style(".background-color") = green;
+    document.getElementById("headsclicked").style(".background-color") = white;
   }
 )}
  
