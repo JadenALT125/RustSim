bce9efa637756c6b222954fb5f865287c9f74cc2
sdoma <82196574+sdomaGitHub@users.noreply.github.com>
2021-05-04 15:45:31 -0400

    Delete scripts directory

--- scripts/headsclicked.js
+++
@@ -1,8 +0,0 @@
-(function headsclicked() {
-  document.getElementById("headsclicked").style.backgroundColor = green;
-  if (document.getElementById("tailsclicked").style.backgroundColor = green) {
-    document.getElementById("headsclicked").style.backgroundColor = green;
-    document.getElementById("tailsclicked").style.backgroundColor = white;
-  }
-)}
- 
--- scripts/tailsclicked.js
+++
@@ -1,8 +0,0 @@
-(function headsclicked() {
-  document.getElementById("tailsclicked").style.backgroundColor = green;
-  if (document.getElementById("headsclicked").style.backgroundColor = green;) {
-    document.getElementById("tailsclicked").style.backgroundColor = green;
-    document.getElementById("headsclicked").style.backgroundColor = white;
-  }
-)}
- 
